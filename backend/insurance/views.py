d6cbe31126753c3ce7c6e2b9e0243ba63778526f
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-07-07 10:32:52 +0000

    make it better

--- backend/insurance/views.py
+++ backend/insurance/views.py
@@ -21,7 +21,8 @@ from payment.function import *
 from user.states import *
 
 
-COVER_AMOUNT=25000000 
+COVER_MULTIPLIER=250000
+COVER_AMOUNT = 250000*10
 BASE_RATE=0.01
 # Create your views here.
 def getTables():
@@ -31,8 +32,8 @@ def getTables():
     riskLoads=pd.read_csv('backend/insurance/RiskLoads.csv',sep=';')
     riskLoads.head()
     riskLoads
-    mortalityTable['ExpectedLossMale']=mortalityTable['Male_Hazard_Rate']*COVER_AMOUNT
-    mortalityTable['ExpectedLossFemale']=mortalityTable['Female_Hazard_Rate']*COVER_AMOUNT
+    mortalityTable['ExpectedLossMale']=mortalityTable['Male_Hazard_Rate']*COVER_MULTIPLIER
+    mortalityTable['ExpectedLossFemale']=mortalityTable['Female_Hazard_Rate']*COVER_MULTIPLIER
     mortalityTable.head()
     return mortalityTable, riskLoads
 
@@ -52,9 +53,9 @@ def getRate(age,gender = 'male',factors = []):
         except:
             print("Error ")
     # Yearly Premium as a percentage of cover amount
-    TotalRate=BASE_RATE+(expectedLoss/COVER_AMOUNT)
+    TotalRate=BASE_RATE+(expectedLoss/COVER_MULTIPLIER)
     # Yearly premium (Expected loss+variable risk)
-    Annual_Premium=(BASE_RATE*COVER_AMOUNT)+expectedLoss
+    Annual_Premium=(BASE_RATE*COVER_MULTIPLIER)+expectedLoss
     return int(Annual_Premium)
 
 def getInsurance(application):
