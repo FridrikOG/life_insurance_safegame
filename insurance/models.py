eeefd6f2ad332822ad094110508eae7c03706365
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-07-10 12:19:38 +0000

    fix not null constraint

--- insurance/models.py
+++ insurance/models.py
@@ -13,6 +13,11 @@ def getCurrentDate():
     ts = calendar.timegm(gmt)
     return ts
 
+def getYearFromNow():
+    expiryOfInsurance = datetime.now(timezone.utc) + relativedelta(years=1)
+    expiryOfInsurance = expiryOfInsurance.strftime("%Y-%m-%d")
+    expiryOfInsurance = datetime.strptime(str(expiryOfInsurance), "%Y-%m-%d")
+    return expiryOfInsurance
 
 
 # Create your models here.
@@ -20,7 +25,7 @@ class Insurance(models.Model):
     application = models.ForeignKey(Application, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     premium = models.IntegerField()
-    dateExpires = models.DateTimeField(max_length=255, default= None)
+    dateExpires = models.DateTimeField(max_length=255, default= getYearFromNow())
     dateCreated = models.DateTimeField(max_length=255, auto_now=True)
     dateApproved = models.DateTimeField(max_length=255, auto_now=True)
     dateModified = models.DateTimeField(max_length=255, auto_now_add=True)
