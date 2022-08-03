ac9b9fe7ce01f29f4d215fa03c558f75c195177c
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-08-03 13:45:14 +0000

    Update settings.py

--- squid/settings.py
+++ squid/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'user.apps.UserConfig',
     'application.apps.ApplicationConfig',
     'insurance.apps.InsuranceConfig',
